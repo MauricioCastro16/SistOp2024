b836f0e13d561ebcf7e8f3d2c22f5dfc97034d59
R-I-O <tiagojlopez@gmail.com>
2024-09-17 09:56:09 -0300

    particiones

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-# Hola, prueba de act 
-# ey
-#prueba pull castro el mas lindo del salon
+particiones = [
+    {"nombre": "Sistema Operativo", "espacio": "100K"},
+    {"nombre": "Trabajos Grandes", "espacio": "250K"},
+    {"nombre": "Trabajos Medianos", "espacio": "150K"},
+    {"nombre": "Trabajos Pequeños", "espacio": "50K"}
+]
+
+	
