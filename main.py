7ba54ccbf6d7f50558a736b36cf2373605582732
Max <maxcocito@gmail.com>
2024-09-17 10:32:45 -0300

    sahfouahessa

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import pandas as pd
+
+# Cargar el archivo CSV en un DataFrame
+df = pd.read_csv('procesos.csv')
+
+# Mostrar las primeras 5 filas
+print(df.head())
+
+
 round_robin = 3
 grado_multiprogramacion = 5
 rendimiento_sistema = 0
