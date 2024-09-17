002a5160263db86dbce14f5585e08228e919c648
AgusCabralC <agustinrobertino@gmail.com>
2024-09-17 16:45:41 -0300

    fixed

--- main.py
+++ main.py
@@ -1,46 +1,48 @@
 
 
 class cpu:
-	def __init__()
+	def __init__(self):
+		print("hello")
 
 class proceso:
-	def __init__(Tr,Ta,Ti,Tam)
-		self.tr = Tr
-		self.ta = Ta
-		self.ti = Ti
-		self.tam = Tam
+	def __init__(self, idproc, tiempArrivo, tiempIrrupt, tamMem):
+		self.idproc = idproc
+		self.tiempArrivo = tiempArrivo
+		self.tiempIrrupt = tiempIrrupt
+		self.tamMem = tamMem
 
 class memoria:
-	def __init__()
+	def __init__(self):
 		self.listaParticiones = []
 		par1 = particion("Sistema Operativo",0,99999)
-		listaParticiones.append(par1)
+		self.listaParticiones.append(par1)
 		par2 = particion("Trabajos Grandes",100000,249999)
-		listaParticiones.append(par2)
+		self.listaParticiones.append(par2)
 		par3 = particion("Trabajos Mediano",350000,149999)
-		listaParticiones.append(par3)
-		par4 = particion("Trabajos Pequeños",500000,49999)
-		listaParticiones.append(par4)
+		self.listaParticiones.append(par3)
+		par4 = particion("Trabajos Pequenos",500000,49999)
+		self.listaParticiones.append(par4)
 
-#(Id de partición, dirección de comienzo de partición, tamaño de la partición, id de proceso asignado a la partición, fragmentación interna
+#(Id de particion, direccion de comienzo de particion, tamano de la particion, id de proceso asignado a la particion, fragmentacion interna
 class particion:
-	def __init__(idM,dirInit,tam):
+	def __init__(self,idM,dirInit,tam):
 		self.idM = idM
 		self.dirInit = dirInit
 		self.tam = tam
 		self.proceso = None
 		self.fragmInt = None
 	
-	def asignarProceso(proceso):
+	def asignarProceso(self, proceso):
 		self.proceso = proceso
 		self.fragmInt = self.tam - proceso.tam
 
-	def liberarProceso():
+	def liberarProceso(self):
 		self.proceso = None
 
-	def estaLibre():
+	def estaLibre(self):
 		return (self.proceso == None)
 
-if __name__ = __main__:
+if __name__ == "__main__":
+	print("Hello world")
 	
 	
